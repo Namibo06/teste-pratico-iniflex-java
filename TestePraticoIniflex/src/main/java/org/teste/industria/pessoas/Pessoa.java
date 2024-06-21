f105b98be48186a49388fdda31cb6e90e49db0e4
Namibo06 <namibo654@gmail.com>
2024-06-20 23:43:58 -0300

    feat: Pessoa

---
+++ TestePraticoIniflex/src/main/java/org/teste/industria/pessoas/Pessoa.java
@@ -0,0 +1,28 @@
+package org.teste.industria.pessoas;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
